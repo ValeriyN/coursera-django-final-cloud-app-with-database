f4579c0fd90b27625621bb5df051d9cec8bb376b
Valerii Nehriiko <negriyko@gmail.com>
2022-11-17 09:36:56 -0500

    next next draft

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -144,8 +144,12 @@ def extract_answers(request):
 def show_exam_result(request, course_id, submission_id):
     context = {}
     course = get_object_or_404(Course, pk=course_id)
+    submission = get_object_or_404(Submission, pk=submission_id)
+    selected_ids = submission.onlinecoursesubmissionchoices_set.all()
     context['course'] = course
     context['grade'] = 10
+    context['selected_ids'] = selected_ids
+    # Question.is_get_score(self, selected_ids)
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 
